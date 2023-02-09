e397feef04c05981d45d40f82a11bda72a99905e
Adam Martin <adam.martin@rancherfederal.com>
2023-02-09 16:19:55 -0500

    initial cosign verify work

--- modules/transfer.py
+++ modules/transfer.py
@@ -1,6 +1,6 @@
 import subprocess
 import sys
-
+from ironbank.pipeline.container_tools.cosign import Cosign
 from .utils.config import Config
 from .utils.image import Image
 from modules.utils import logger
@@ -13,28 +13,39 @@ class Transfer:
         self.registry: str = config.destination["registry"]
         self.secure: bool = config.destination["secure"]
         self.images: [Image] = config.images
+        self.cosign_pubkey = config.cosign_pubkey
+
+    @classmethod
+    def _cosign_verify(cls, image: Image):
+        if image.registry() == "registry1.dso.mil" and image.repo().split("/")[0] == "ironbank":
+            return Cosign.verify(image.name, cls.cosign_pubkey, log_cmd=True)
+        else: 
+            log.info("Skipping verification of image %s", image)
+            return True
 
     def execute(self):
         total_images = len(self.images)
         count = 0
         for source in self.images:
             count += 1
-            destination = Image.new_registry(source, self.registry, self.secure)
-            cmd = [
-                "skopeo",
-                "copy",
-                f"docker://{source.name}",
-                f"docker://{destination.name}",
-            ]
+            if self._cosign_verify(source):
+                destination = Image.new_registry(source, self.registry, self.secure)
+                cmd = [
+                    "skopeo",
+                    "copy",
+                    f"docker://{source.name}",
+                    f"docker://{destination.name}",
+                ]
 
-            cmd += ["--dest-tls-verify=false"] if not self.secure else []
+                cmd += ["--dest-tls-verify=false"] if not self.secure else []
 
-            log.info(f"[{count}/{total_images}] Copying {source} to {destination}")
-            source_digest = source.digest()
-            destination_digest = destination.digest()
-            if not source_digest or not destination_digest or (source_digest != destination_digest):
-                copy_result = subprocess.run(args=cmd, capture_output=True, check=True)
-                log.info(copy_result.stdout.decode())
+                log.info(f"[{count}/{total_images}] Copying {source} to {destination}")
+                source_digest = source.digest()
+                destination_digest = destination.digest()
+                if not source_digest or not destination_digest or (source_digest != destination_digest):
+                    copy_result = subprocess.run(args=cmd, capture_output=True, check=True)
+                    log.info(copy_result.stdout.decode())
+                else:
+                    log.info("Source and destination digests match, skipping sync for %s", source.name)
             else:
-                log.info("Source and destination digests match, skipping sync for %s", source.name)
-
+                log.info("Image %s failed verification!!", source)
